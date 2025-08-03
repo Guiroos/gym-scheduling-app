7d5a92dd85770008a843643eaa62598b801d5a83
Guilherme Roos <guilherme.roosr@gmail.com>
2025-08-03 13:48:46 -0300

    feat: add hooks folder to lint import and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
       { find: '@/', replacement: path.resolve(__dirname, 'src') },
       { find: '@/assets', replacement: path.resolve(__dirname, 'src/assets') },
       { find: '@/components', replacement: path.resolve(__dirname, 'src/components') },
+      { find: '@/hooks', replacement: path.resolve(__dirname, 'src/hooks') },
       { find: '@/customTypes', replacement: path.resolve(__dirname, 'src/types') },
       { find: '@/pages', replacement: path.resolve(__dirname, 'src/pages') },
       { find: '@/routes', replacement: path.resolve(__dirname, 'src/routes') },
